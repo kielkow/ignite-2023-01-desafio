65f14a62e5392620ff5ac16fcb4b03db9a0b6e30
kielkow <matheuskiel@fiorifer.com.br>
2023-05-15 15:11:47 -0300

    feat: import tasks from csv

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
+import fs from 'node:fs';
 import { randomUUID } from 'node:crypto';
+import { parse } from 'csv-parse';
 
 import { Database } from './database.js';
 import { buildRoutePath } from './utils/build-route-path.js';
@@ -28,20 +30,49 @@ export const routes = [
         method: 'POST',
         path: buildRoutePath('/tasks'),
         handler: (req, res) => {
-            const { title, description } = req.body;
+            if (!req.body && fs.existsSync('./tasks.csv')) {
+                fs.createReadStream('./tasks.csv')
+                    .pipe(
+                        parse({ delimiter: ",", from_line: 2 }) // pula a primeira linha
+                    )
+                    .on(
+                        'data', (task) => {
+                            database.insert('tasks', {
+                                id: randomUUID(),
+                                title: task[0],
+                                description: task[1],
+                                created_at: new Date().toISOString(),
+                                updated_at: new Date().toISOString(),
+                                completed_at: null,
+                            });
+                        }
+                    )
+                    .on("end", function () {
+                        return res.writeHead(201).end(
+                            JSON.stringify({ message: 'tasks imported' })
+                        );
+                    })
+                    .on("error", function (error) {
+                        return res.writeHead(400).end(JSON.stringify({
+                            message: error.message || error
+                        }));
+                    });
+            } else {
+                const { title, description } = req.body;
 
-            const task = {
-                id: randomUUID(),
-                title,
-                description,
-                created_at: new Date().toISOString(),
-                updated_at: new Date().toISOString(),
-                completed_at: null,
-            };
+                const task = {
+                    id: randomUUID(),
+                    title,
+                    description,
+                    created_at: new Date().toISOString(),
+                    updated_at: new Date().toISOString(),
+                    completed_at: null,
+                };
 
-            database.insert('tasks', task);
+                database.insert('tasks', task);
 
-            return res.writeHead(201).end();
+                return res.writeHead(201).end();
+            }
         },
     },
     {
@@ -69,8 +100,8 @@ export const routes = [
                 }));
             }
 
-            database.update('tasks', id, { 
-                title, 
+            database.update('tasks', id, {
+                title,
                 description,
                 created_at: task.created_at,
                 updated_at: new Date().toISOString(),
@@ -111,8 +142,8 @@ export const routes = [
                 }));
             }
 
-            database.update('tasks', id, { 
-                title: task.title, 
+            database.update('tasks', id, {
+                title: task.title,
                 description: task.description,
                 created_at: task.created_at,
                 updated_at: new Date().toISOString(),
